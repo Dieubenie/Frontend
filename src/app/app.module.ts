1e9af4796bee0da06683cafab244321a354c0f46
Bethel Ngakoutou <ngaradoumbethel@gmail.com>
2022-12-16 15:01:31 +0000

    L'accueil de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { EvenementsComponent } from './components/evenements/evenements.component';
 import { TicketsComponent } from './components/tickets/tickets.component';
+import { UsersComponent } from './components/users/users.component';
+import { AchatsComponent } from './components/achats/achats.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CategoriesComponent,
     EvenementsComponent,
-    TicketsComponent
+    TicketsComponent,
+    UsersComponent,
+    AchatsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
